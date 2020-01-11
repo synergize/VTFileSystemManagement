babf3cea3393787b630dd4dd8b591c9f66a893d5
synergize <err0r142@gmail.com>
2020-01-10 20:53:31 -0800

    Added notation. Feature complete.

--- VTFileSystemManagement/FileSystemManager.cs
+++ VTFileSystemManagement/FileSystemManager.cs
@@ -3,9 +3,16 @@ using System.IO;
 
 namespace VTFileSystemManagement
 {
+    /// <summary>
+    /// Basic library setup to read and write JSON documents from within a Data Directory in the base location of a program. 
+    /// </summary>
     public class FileSystemManager
     {
         private string _DataDirectory = Path.Combine(Path.GetFullPath(Directory.GetCurrentDirectory()), "Data");
+
+        /// <summary>
+        /// Constructor makes sure a "Data" Directory as JSON documents are saved here by default in this library. 
+        /// </summary>
         public FileSystemManager()
         {
             if (!Directory.Exists(_DataDirectory))
@@ -13,6 +20,25 @@ namespace VTFileSystemManagement
                 Directory.CreateDirectory(_DataDirectory);
             }
         }
+
+        /// <summary>
+        /// Constructor overload that allows a user to create their own directory within the root location of the program using this library. 
+        /// </summary>
+        /// <param name="createDirectoryName"></param>
+        public FileSystemManager(string createDirectoryName)
+        {
+            if (!Directory.Exists(createDirectoryName))
+            {
+                Directory.CreateDirectory(createDirectoryName);
+            }
+        }
+
+        /// <summary>
+        /// Reads a JSON file by file name and returns it in a string format. 
+        /// This defaults it's location inside directory <see cref="_DataDirectory"/>
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
         public string ReadJsonFile(string fileName)
         {
             string file = BuildFilePath(fileName);
@@ -24,25 +50,25 @@ namespace VTFileSystemManagement
             return null;            
         }
 
-        public string GetFilePath(string file)
-        {
-            return BuildFilePath(file);
-        }
-
+        /// <summary>
+        /// Saves an object to a JSON file by serializing using NewtonSoft.
+        /// This defaults it's location to <see cref="_DataDirectory"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="jsonData"></param>
+        /// <param name="fileName"></param>
         public void SaveJsonFile<T>(T jsonData, string fileName)
         {
-            if (File.Exists(BuildFilePath(fileName)))
+            using (StreamWriter file = File.CreateText(BuildFilePath(fileName)))
             {
-                using (StreamWriter file = File.CreateText(BuildFilePath(fileName)))
+                var serializer = new JsonSerializer
                 {
-                    var serializer = new JsonSerializer
-                    {
-                        Formatting = Formatting.Indented
+                    Formatting = Formatting.Indented
 
-                    };
-                    serializer.Serialize(file, jsonData);
-                }
+                };
+                serializer.Serialize(file, jsonData);
             }
+
         }
 
         public bool IsFileExists(string fileName)
@@ -50,6 +76,11 @@ namespace VTFileSystemManagement
             return File.Exists(BuildFilePath(fileName));
         }
 
+        /// <summary>
+        /// Conbines <see cref="file"/> and <see cref="_DataDirectory"/> to create the full file path of a text file by name. 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
         private string BuildFilePath(string file)
         {
             return Path.Combine(_DataDirectory, file);
